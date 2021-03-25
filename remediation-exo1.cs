02ae88d644719adda9bdec1f59a6d1c3f8ff4f79
Yann Martin <ymartin@la-providence.net>
2021-03-25 16:34:13 +0100

    Exo 1 remediation

---
+++ remediation-exo1.cs
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Point3D[49][3];
+    int i, j, p;
+
+    for (i = 0; i < 49; i++)
+    {
+        Point3D[i][0] = 0;
+        Point3D[i][1] = 0;
+        Point3D[i][2] = 0;
+    }
+
+
+    printf("\nTableau : \n\n");
+
+    printf("Entrez les 10 premires valeurs : \n");
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("\nPoint %d :\n", i + 1);
+
+        printf("\tx = ");
+        scanf("%d", &Point3D[i][0]);
+
+        printf("\ty = ");
+        scanf("%d", &Point3D[i][1]);
+
+        printf("\tz = ");
+        scanf("%d", &Point3D[i][2]);
+
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("\nPoint %d : ", i + 1);
+        printf("x= %d  y= %d  z= %d  ", Point3D[i][0], Point3D[i][1], Point3D[i][2]);
+    }
+
+}
