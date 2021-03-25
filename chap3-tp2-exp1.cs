84f4522e589039150afeece6f697e3ea2a0e33b0
Yann Martin <ymartin@la-providence.net>
2021-03-25 15:27:48 +0100

    Ajout TP2 CHAP3 EXO1

---
+++ chap3-tp2-exp1.cs
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void conversion(char* line, char* resultat);
+
+int main(void)
+{
+    char line[255];
+    char resultat[255];
+
+    printf("Entrez une phrase: ");
+    scanf("%s", line);
+
+    conversion(line, resultat);
+    printf("%s", resultat);
+
+    return (0);
+}
+
+void conversion(char* line, char* resultat)
+{
+    int maj = 0;
+    int i;
+    for (i = 0; i < strlen(line); i++)
+    {
+
+        resultat[i] = line[i];
+        if (resultat[i] >= 'A' && resultat[i] <= 'Z')
+        {
+            resultat[i] += 32;
+            maj++;
+        }
+    }
+    resultat[i] = 0;    // Termine la chaîne résultat.
+    printf("Voici votre texte apres la conversion de %d lettres majuscules en minuscules :", maj);
+
+}
